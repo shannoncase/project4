f66f16963467deb174d7d1a5b42fe16b9e7b533e
shannoncase <scase@andrew.cmu.edu>
2017-11-15 01:14:56 -0500

    die

--- main.js
+++ main.js
@@ -8,6 +8,39 @@ var mainState = {
         game.load.image('pipe', 'assets/pipe.png');
     },
 
+    addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe 
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200; 
+
+    // Automatically kill the pipe when it's no longer visible 
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+
+    this.score += 1;
+    this.labelScore.text = this.score;
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes 
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1) 
+            this.addOnePipe(400, i * 60 + 10);   
+    },
+
     create: function() { 
 
     this.score = 0;
@@ -44,39 +77,6 @@ var mainState = {
         // Here we set up the game, display sprites, etc.  
     },
 
-    addOnePipe: function(x, y) {
-    // Create a pipe at the position x and y
-    var pipe = game.add.sprite(x, y, 'pipe');
-
-    // Add the pipe to our previously created group
-    this.pipes.add(pipe);
-
-    // Enable physics on the pipe 
-    game.physics.arcade.enable(pipe);
-
-    // Add velocity to the pipe to make it move left
-    pipe.body.velocity.x = -200; 
-
-    // Automatically kill the pipe when it's no longer visible 
-    pipe.checkWorldBounds = true;
-    pipe.outOfBoundsKill = true;
-    },
-
-    addRowOfPipes: function() {
-
-    this.score += 1;
-    this.labelScore.text = this.score;
-    // Randomly pick a number between 1 and 5
-    // This will be the hole position
-    var hole = Math.floor(Math.random() * 5) + 1;
-
-    // Add the 6 pipes 
-    // With one big hole at position 'hole' and 'hole + 1'
-    for (var i = 0; i < 8; i++)
-        if (i != hole && i != hole + 1) 
-            this.addOnePipe(400, i * 60 + 10);   
-    },
-
 
     update: function() {
 
@@ -102,7 +102,7 @@ var mainState = {
     restartGame: function() {
          // Start the 'main' state, which restarts the game
         game.state.start('main');
-    }
+    },
 };
 
 // Initialize Phaser, and create a 400px by 490px game
