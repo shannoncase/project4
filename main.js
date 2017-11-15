5ce561e1af09a4b232f9aa6484b77407d352c08d
shannoncase <scase@andrew.cmu.edu>
2017-11-15 01:08:40 -0500

    die

--- main.js
+++ main.js
@@ -19,10 +19,32 @@ var mainState = {
     this.pipes = game.add.group(); 
 
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+            // Change the background color of the game to blue
+    game.stage.backgroundColor = '#71c5cf';
+
+    // Set the physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Display the bird at the position x=100 and y=245
+    this.bird = game.add.sprite(100, 245, 'bird');
+
+    // Add physics to the bird
+    // Needed for: movements, gravity, collisions, etc.
+    game.physics.arcade.enable(this.bird);
+
+    // Add gravity to the bird to make it fall
+    this.bird.body.gravity.y = 1000;  
+
+    // Call the 'jump' function when the spacekey is hit
+    var spaceKey = game.input.keyboard.addKey(
+                    Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);    
+        // This function is called after the preload function     
+        // Here we set up the game, display sprites, etc.  
     },
 
-    
-    addOnePipe: function(x, y) {
+        addOnePipe: function(x, y) {
     // Create a pipe at the position x and y
     var pipe = game.add.sprite(x, y, 'pipe');
 
@@ -53,30 +75,6 @@ var mainState = {
     for (var i = 0; i < 8; i++)
         if (i != hole && i != hole + 1) 
             this.addOnePipe(400, i * 60 + 10);   
-    },
-
-            // Change the background color of the game to blue
-    game.stage.backgroundColor = '#71c5cf';
-
-    // Set the physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    // Display the bird at the position x=100 and y=245
-    this.bird = game.add.sprite(100, 245, 'bird');
-
-    // Add physics to the bird
-    // Needed for: movements, gravity, collisions, etc.
-    game.physics.arcade.enable(this.bird);
-
-    // Add gravity to the bird to make it fall
-    this.bird.body.gravity.y = 1000;  
-
-    // Call the 'jump' function when the spacekey is hit
-    var spaceKey = game.input.keyboard.addKey(
-                    Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);    
-        // This function is called after the preload function     
-        // Here we set up the game, display sprites, etc.  
     },
 
 
